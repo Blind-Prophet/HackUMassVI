5b02359c18782a20b585f0016d16495b17c1d2cd
Blind-Prophet <darkewizzard@gmail.com>
2018-10-13 13:04:33 -0400

    arduino is working!!

--- ArduinoCode/arduino_main/arduino_main.ino
+++ ArduinoCode/arduino_main/arduino_main.ino
@@ -1,10 +1,30 @@
+#include <Adafruit_SSD1306.h>
+Adafruit_SSD1306 lcd(-1); // create display object
+
 void setup(){
   Serial.begin(9600);
+  lcd.begin(SSD1306_SWITCHCAPVCC, 0x3C); // init LCD
+  lcd.clearDisplay();
+  lcd.setTextColor(WHITE);
+  lcd.setTextSize(1);
+  lcd.display();
 }
+
 void loop(){
-  char c;
-  if(Serial.available()){
-    c = Serial.read();
-    Serial.print(c);
+  lcd.clearDisplay();
+  lcd.setCursor(0,0);
+  
+  String content = "";
+  char character;
+
+  while(Serial.available()) {
+      character = Serial.read();
+      content.concat(character);
   }
+
+  if (content != "") {
+    lcd.print(content);
+  }
+
+  lcd.display();
 }
