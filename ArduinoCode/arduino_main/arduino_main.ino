7b1056c3e21eec123dec7d1f3ab2f3c58bb6542a
Blind-Prophet <darkewizzard@gmail.com>
2018-10-13 10:23:56 -0400

    added code

--- ArduinoCode/arduino_main/arduino_main.ino
+++ ArduinoCode/arduino_main/arduino_main.ino
@@ -1,9 +1,10 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup(){
+  Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  char c;
+  if(Serial.available()){
+    c = Serial.read();
+    Serial.print(c);
+  }
 }
